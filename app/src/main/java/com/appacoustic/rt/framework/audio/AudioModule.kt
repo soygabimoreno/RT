3c75c2fb63e96e41296a1988063e3ead5ddad7ca
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-09-30 19:06:55 +0200

    Recording is saved properly

--- app/src/main/java/com/appacoustic/rt/framework/audio/AudioModule.kt
+++ app/src/main/java/com/appacoustic/rt/framework/audio/AudioModule.kt
@@ -1,28 +1,15 @@
 package com.appacoustic.rt.framework.audio
 
-import android.media.AudioRecord
 import com.appacoustic.rt.framework.audio.recorder.Recorder
+import com.appacoustic.rt.framework.audio.recorder.WavHeaderWriter
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val audioModule = module {
     single {
-        AudioRecord(
-            Recorder.AUDIO_SOURCE,
-            Recorder.SAMPLE_RATE,
-            Recorder.CHANNELS,
-            Recorder.AUDIO_ENCODING,
-            AudioRecord.getMinBufferSize(
-                Recorder.SAMPLE_RATE,
-                Recorder.CHANNELS,
-                Recorder.AUDIO_ENCODING
-            )
-        )
-    }
-    single {
         Recorder(
             context = androidContext(),
-            audioRecord = get()
+            wavHeaderWriter = WavHeaderWriter()
         )
     }
 }
--- app/src/main/java/com/appacoustic/rt/framework/audio/recorder/Recorder.kt
+++ app/src/main/java/com/appacoustic/rt/framework/audio/recorder/Recorder.kt
@@ -3,57 +3,85 @@ package com.appacoustic.rt.framework.audio.recorder
 import android.content.Context
 import android.media.AudioFormat
 import android.media.AudioRecord
+import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.net.Uri
 import com.appacoustic.rt.framework.KLog
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 
 class Recorder(
     private val context: Context,
-    private val audioRecord: AudioRecord
+    private val wavHeaderWriter: WavHeaderWriter
 ) {
 
     companion object {
         const val AUDIO_SOURCE = MediaRecorder.AudioSource.MIC
         const val SAMPLE_RATE = 44100
         const val CHANNELS = AudioFormat.CHANNEL_IN_MONO
+        const val N_CHANNELS = 1
         const val AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT
         private const val BITS_PER_SAMPLE = 16
+        private const val BYTE_RATE = BITS_PER_SAMPLE * SAMPLE_RATE * N_CHANNELS / 8
 
-        private const val FILE_EXTENSION = ".wav"
-        private const val FOLDER_MAIN = "RT" // TODO: If finally we use it internally, it does not make sense to call this way: "Records" or similar
-        private const val FOLDER_CSV = "$FOLDER_MAIN/CSV"
-        private const val FILENAME_TEMP = "record_temp.raw"
+        private const val EXTENSION_WAV = ".wav"
+        private const val FOLDER_RECORDINGS = "RECORDINGS"
+        private const val FOLDER_CSV = "CSV"
     }
 
-    private var totalAudioLength = 0
+    private var tempPath = getTempPath()
+    private var path = getPath()
+
+    private lateinit var audioRecord: AudioRecord
+    private var bufferSize = 0
+    private var totalAudioLength = 0L
+
     private lateinit var xBytes: ByteArray
 
     private var recording = false
 
     fun start() {
+        audioRecord = buildAudioRecord()
+        bufferSize = audioRecord.bufferSizeInFrames
         KLog.d("audioRecord: $audioRecord")
         audioRecord.startRecording()
         recording = true
+        GlobalScope.launch {
+            writeRawTempFile()
+        }
+    }
+
+    private fun buildAudioRecord() = AudioRecord(
+        AUDIO_SOURCE,
+        SAMPLE_RATE,
+        CHANNELS,
+        AUDIO_ENCODING,
+        AudioRecord.getMinBufferSize(
+            SAMPLE_RATE,
+            CHANNELS,
+            AUDIO_ENCODING
+        )
+    )
+
+    fun stop() {
+        recording = false
+        audioRecord.stop()
+        audioRecord.release()
+
         GlobalScope.launch {
             writeWavFile()
         }
     }
 
-    fun stop() {
-        recording = false
-    }
-
-    private suspend fun writeWavFile() = coroutineScope {
+    private suspend fun writeRawTempFile() = coroutineScope {
         launch {
-            val tempFilename = getTempFilename()
-            KLog.d("tempFilename: $tempFilename")
+            KLog.d("tempPath: $tempPath")
 
-            val fos = FileOutputStream(tempFilename)
-            val bufferSize = audioRecord.bufferSizeInFrames
+            val fos = FileOutputStream(tempPath)
             val data = ByteArray(bufferSize)
             while (recording) {
                 val read = audioRecord.read(data, 0, bufferSize)
@@ -65,20 +93,59 @@ class Recorder(
         }
     }
 
-    private fun getTempFilename(): String {
-//        val filePath = Environment.getExternalStorageDirectory().path // ERASE
+    private fun getTempPath(): String {
         val filePath = context.filesDir
-        val file = File(filePath, FOLDER_MAIN)
+        val file = File(filePath, FOLDER_RECORDINGS)
 
         if (!file.exists()) {
             file.mkdirs()
         }
 
-        val tempFile = File(filePath, FILENAME_TEMP)
+        val tempFilename = "record_temp.raw"
+        val tempFile = File(filePath, tempFilename)
         if (tempFile.exists()) {
             tempFile.delete()
         }
 
-        return file.absolutePath + "/" + FILENAME_TEMP
+        return file.absolutePath + "/" + tempFilename
+    }
+
+    private fun getPath(): String {
+        val filePath = context.filesDir
+        val file = File(filePath, FOLDER_RECORDINGS)
+
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+
+        return file.absolutePath + "/" + System.currentTimeMillis() + EXTENSION_WAV
+    }
+
+    private suspend fun writeWavFile() = coroutineScope {
+        launch {
+            val fis = FileInputStream(tempPath)
+            val fos = FileOutputStream(path)
+
+            val data = ByteArray(bufferSize)
+            totalAudioLength = fis.channel.size()
+            val totalDataLength = totalAudioLength + 36
+            wavHeaderWriter(
+                fos = fos,
+                totalAudioLength = totalAudioLength,
+                totalDataLength = totalDataLength,
+                sampleRate = SAMPLE_RATE,
+                nChannels = N_CHANNELS,
+                byteRate = BYTE_RATE,
+                bitPerSample = BITS_PER_SAMPLE
+            )
+            while (fis.read(data) != -1) {
+                fos.write(data)
+            }
+            fis.close()
+            fos.close()
+
+            val mediaPlayer = MediaPlayer.create(context, Uri.parse(path))
+            mediaPlayer.start()
+        }
     }
 }
