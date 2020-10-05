b022011d41f5d2ad5e2d76b4a9e289e68659930b
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-10-05 07:22:48 +0200

    Testing the algorithm

---
+++ app/src/androidTest/java/com/appacoustic/rt/domain/calculator/ReverbTimeCalculatorTest.kt
@@ -0,0 +1,60 @@
+package com.appacoustic.rt.domain.calculator
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.appacoustic.rt.R
+import com.appacoustic.rt.framework.KLog
+import com.appacoustic.rt.framework.audio.recorder.Recorder
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ReverbTimeCalculatorTest {
+
+    /**
+     * We have a reference measures made by some MATLAB scripts and executed in Octave.
+     *
+     * These are the input values:
+     * - audioFile: "clap.wav" (inside assets)
+     * - dBStart: -5 dB
+     * - dBEnd: -15 dB
+     *
+     * The measures are the following ones:
+     * - 125 Hz: 0.31878
+     * - 250 Hz: 0.35184
+     * - 500 Hz: 0.35932
+     * - 1000 Hz: 0.39510
+     * - 2000 Hz: 0.39429
+     * - 4000 Hz: 0.33143
+     */
+
+    @Test
+    fun if_a_wav_file_read_is_used_as_input_byte_array_then_the_measures_are_the_expected_ones() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+        val inputStream = context.resources.openRawResource(R.raw.clap)
+        val xBytes = inputStream.readBytes()
+
+        val reverbTimeCalculator = ReverbTimeCalculator()
+        reverbTimeCalculator(xBytes, Recorder.SAMPLE_RATE)
+            .fold({
+                KLog.e("exception: ${it.message}")
+                fail()
+            }, { measures ->
+                val rt125 = measures[0].time
+                val rt250 = measures[1].time
+                val rt500 = measures[2].time
+                val rt1000 = measures[3].time
+                val rt2000 = measures[4].time
+                val rt4000 = measures[5].time
+                assertTrue(0.31878f == rt125)
+                assertTrue(0.35184f == rt250)
+                assertTrue(0.35932f == rt500)
+                assertTrue(0.39510f == rt1000)
+                assertTrue(0.39429f == rt2000)
+                assertTrue(0.33143f == rt4000)
+            })
+    }
+}
--- app/src/main/java/com/appacoustic/rt/domain/calculator/ReverbTimeCalculator.kt
+++ app/src/main/java/com/appacoustic/rt/domain/calculator/ReverbTimeCalculator.kt
@@ -22,45 +22,49 @@ class ReverbTimeCalculator {
         return if (xBytes.isEmpty()) {
             EmptyByteArraySignalException().left()
         } else {
-            val x = xBytes
-                .toDoubleSamples()
-                .windowingSignal(300, 100)
-                .toDivisibleBy32()
-                .normalize()
+            try {
+                val x = xBytes
+                    .toDoubleSamples()
+                    .windowingSignal(300, 100)
+                    .toDivisibleBy32()
+                    .normalize()
 
-            if (x.isEmpty()) {
-                EmptyDoubleArraySignalException().left()
-            } else {
-                val dBStart = ReverbTimeMethod.EDT.dBStart
-                val dBEnd = ReverbTimeMethod.EDT.dBEnd
-                val positionDbStart125 = x.calculatePosition(ButterworthCoefficientsOrder2.FREQUENCY_125, dBStart)
-                val positionDbEnd125 = x.calculatePosition(ButterworthCoefficientsOrder2.FREQUENCY_125, dBEnd)
-                val positionDbStart250 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_250, dBStart)
-                val positionDbEnd250 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_250, dBEnd)
-                val positionDbStart500 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_500, dBStart)
-                val positionDbEnd500 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_500, dBEnd)
-                val positionDbStart1000 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_1000, dBStart)
-                val positionDbEnd1000 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_1000, dBEnd)
-                val positionDbStart2000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_2000, dBStart)
-                val positionDbEnd2000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_2000, dBEnd)
-                val positionDbStart4000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_4000, dBStart)
-                val positionDbEnd4000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_4000, dBEnd)
+                if (x.isEmpty()) {
+                    EmptyDoubleArraySignalException().left()
+                } else {
+                    val dBStart = ReverbTimeMethod.EDT.dBStart
+                    val dBEnd = ReverbTimeMethod.EDT.dBEnd
+                    val positionDbStart125 = x.calculatePosition(ButterworthCoefficientsOrder2.FREQUENCY_125, dBStart)
+                    val positionDbEnd125 = x.calculatePosition(ButterworthCoefficientsOrder2.FREQUENCY_125, dBEnd)
+                    val positionDbStart250 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_250, dBStart)
+                    val positionDbEnd250 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_250, dBEnd)
+                    val positionDbStart500 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_500, dBStart)
+                    val positionDbEnd500 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_500, dBEnd)
+                    val positionDbStart1000 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_1000, dBStart)
+                    val positionDbEnd1000 = x.calculatePosition(ButterworthCoefficientsOrder4.FREQUENCY_1000, dBEnd)
+                    val positionDbStart2000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_2000, dBStart)
+                    val positionDbEnd2000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_2000, dBEnd)
+                    val positionDbStart4000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_4000, dBStart)
+                    val positionDbEnd4000 = x.calculatePosition(ButterworthCoefficientsOrder8.FREQUENCY_4000, dBEnd)
 
-                val reverbTime125 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd125 - positionDbStart125).toDouble() / sampleRate
-                val reverbTime250 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd250 - positionDbStart250).toDouble() / sampleRate
-                val reverbTime500 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd500 - positionDbStart500).toDouble() / sampleRate
-                val reverbTime1000 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd1000 - positionDbStart1000).toDouble() / sampleRate
-                val reverbTime2000 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd2000 - positionDbStart2000).toDouble() / sampleRate
-                val reverbTime4000 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd4000 - positionDbStart4000).toDouble() / sampleRate
+                    val reverbTime125 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd125 - positionDbStart125).toDouble() / sampleRate
+                    val reverbTime250 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd250 - positionDbStart250).toDouble() / sampleRate
+                    val reverbTime500 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd500 - positionDbStart500).toDouble() / sampleRate
+                    val reverbTime1000 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd1000 - positionDbStart1000).toDouble() / sampleRate
+                    val reverbTime2000 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd2000 - positionDbStart2000).toDouble() / sampleRate
+                    val reverbTime4000 = 60 / (-dBEnd + dBStart).toDouble() * (positionDbEnd4000 - positionDbStart4000).toDouble() / sampleRate
 
-                listOf(
-                    Measure(Measure.Frequency.FREQUENCY_125, reverbTime125.toFloat()),
-                    Measure(Measure.Frequency.FREQUENCY_250, reverbTime250.toFloat()),
-                    Measure(Measure.Frequency.FREQUENCY_500, reverbTime500.toFloat()),
-                    Measure(Measure.Frequency.FREQUENCY_1000, reverbTime1000.toFloat()),
-                    Measure(Measure.Frequency.FREQUENCY_2000, reverbTime2000.toFloat()),
-                    Measure(Measure.Frequency.FREQUENCY_4000, reverbTime4000.toFloat())
-                ).right()
+                    listOf(
+                        Measure(Measure.Frequency.FREQUENCY_125, reverbTime125.toFloat()),
+                        Measure(Measure.Frequency.FREQUENCY_250, reverbTime250.toFloat()),
+                        Measure(Measure.Frequency.FREQUENCY_500, reverbTime500.toFloat()),
+                        Measure(Measure.Frequency.FREQUENCY_1000, reverbTime1000.toFloat()),
+                        Measure(Measure.Frequency.FREQUENCY_2000, reverbTime2000.toFloat()),
+                        Measure(Measure.Frequency.FREQUENCY_4000, reverbTime4000.toFloat())
+                    ).right()
+                }
+            } catch (e: Exception) {
+                e.left()
             }
         }
     }
