a9cb50e6eb4b0a82fc77a9d87a9e2defec440232
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2025-05-08 13:03:38 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.appacoustic.rt"
         minSdk = 23
         targetSdk = 35
-        versionCode = 37
-        versionName = "4.0.0"
+        versionCode = 38
+        versionName = "4.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
